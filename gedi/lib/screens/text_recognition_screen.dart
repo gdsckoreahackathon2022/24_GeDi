5e234d31467248d1f3f570fad18ae4c56298b3b8
DevLuce <whchoi000@gmail.com>
2022-02-04 21:16:41 +0900

    Add image recognition ML

--- gedi/lib/screens/text_recognition_screen.dart
+++ gedi/lib/screens/text_recognition_screen.dart
@@ -3,8 +3,9 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:gedi/screens/image_view_screen.dart';
 import 'package:gedi/widgets/image_round_container.dart';
+import 'package:google_ml_kit_for_korean/google_ml_kit_for_korean.dart';
 
-class TextRecognitionScreen extends StatelessWidget {
+class TextRecognitionScreen extends StatefulWidget {
   const TextRecognitionScreen({Key? key, required this.image})
       : super(key: key);
   final File image;
@@ -18,6 +19,29 @@ class TextRecognitionScreen extends StatelessWidget {
         ),
       ),
     );
+  }
+
+  @override
+  State<TextRecognitionScreen> createState() => _TextRecognitionScreenState();
+}
+
+class _TextRecognitionScreenState extends State<TextRecognitionScreen> {
+  late InputImage visionImage;
+  TextDetectorV2 textDetector = GoogleMlKit.vision.textDetectorV2();
+  late RecognisedText visionText;
+
+  @override
+  void initState() {
+    super.initState();
+    visionImage = InputImage.fromFile(widget.image);
+  }
+
+  Future<String> recognized() async {
+    // visionText = await textRecognizer.processImage(visionImage);
+    visionText = await textDetector.processImage(visionImage);
+    String text = visionText.text;
+
+    return text;
   }
 
   @override
@@ -39,10 +63,10 @@ class TextRecognitionScreen extends StatelessWidget {
                 height: MediaQuery.of(context).size.height / 3.5,
                 margin:
                     const EdgeInsets.symmetric(horizontal: 10, vertical: 25),
-                image: FileImage(image),
+                image: FileImage(widget.image),
                 onPressed: () => ImageViewScreen.show(
                   context,
-                  image: FileImage(image),
+                  image: FileImage(widget.image),
                 ),
               ),
             ),
@@ -58,25 +82,58 @@ class TextRecognitionScreen extends StatelessWidget {
                       ),
                     ),
                   ),
-                  Column(
-                    children: const <Widget>[
-                      LetterInImageContainer(
-                        title: "사진 속에서 찾아낸 글자",
-                        content: "맘스스테이션",
-                      ),
-                      Expanded(
-                        child: CorrectWordsContainer(
-                          title: "잘못된 단어 수정",
-                        ),
-                      )
-                    ],
-                  )
+                  FutureBuilder(
+                    future: recognized(),
+                    builder: (BuildContext context, AsyncSnapshot snapshot) {
+                      if (snapshot.hasData == false) {
+                        return const RecognizedWordsContainer(
+                            content: "No Data");
+                      } else if (snapshot.hasError) {
+                        return RecognizedWordsContainer(
+                            content: 'Error: ${snapshot.error}');
+                      } else {
+                        String content = snapshot.data.toString();
+                        if (content == "") {
+                          return const RecognizedWordsContainer(
+                              content: "발견한 단어가 없습니다.");
+                        } else {
+                          return RecognizedWordsContainer(content: content);
+                        }
+                      }
+                    },
+                  ),
                 ],
               ),
             )
           ],
         ),
       ),
+    );
+  }
+}
+
+class RecognizedWordsContainer extends StatelessWidget {
+  const RecognizedWordsContainer({
+    Key? key,
+    required this.content,
+  }) : super(key: key);
+
+  final String content;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        LetterInImageContainer(
+          title: "사진 속에서 찾아낸 글자",
+          content: content,
+        ),
+        const Expanded(
+          child: CorrectWordsContainer(
+            title: "잘못된 단어 수정",
+          ),
+        )
+      ],
     );
   }
 }
